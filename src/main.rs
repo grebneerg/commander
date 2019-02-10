145aaaf0da830f912f58c6b5aa8cb2d70bd61a7d
Jack Greenberg <theProgrammerJack@gmail.com>
2019-02-09 21:20:08 -0500

    Functioning drive with joysticks

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{thread, time};
 use wpilib::{
+    dio::DigitalInput,
     ds::{JoystickAxis, JoystickPort, RobotState},
     pwm::PwmSpeedController,
     RobotBase,
@@ -21,6 +22,8 @@ fn main() {
     let x_axis = JoystickAxis::new(0).expect("bad axis");
     let y_axis = JoystickAxis::new(1).expect("bad axis");
 
+    let front_bumper = DigitalInput::new(0).expect("oh no");
+
     // These are actually some old model of victors but it seems to work.
     let mut right1 = PwmSpeedController::new_talon(0).expect("failed making victor");
     let mut right2 = PwmSpeedController::new_talon(1).expect("failed making victor");
@@ -32,18 +35,31 @@ fn main() {
     loop {
         let state = ds.robot_state();
 
-        let left = 0.5
+        let mut left = 0.5
             * if let Ok(val) = ds.stick_axis(left_stick, y_axis) {
                 val
             } else {
                 0.0
             };
-        let right = 0.5
+        let mut right = 0.5
             * if let Ok(val) = ds.stick_axis(right_stick, y_axis) {
                 val
             } else {
                 0.0
             };
+
+        println!("Switch pos {}", front_bumper.get().unwrap_or(true));
+
+        // if front_bumper.get().unwrap_or(true) {
+        //     if left < 0 {
+        //         left = 0
+        //     }
+        //     if right < 0 {
+        //         right = 0
+        //     }
+        // }
+
+        println!("l:{},r:{}", left, right);
 
         match state {
             RobotState::Teleop => {
